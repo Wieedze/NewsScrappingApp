a2a4485a558f3812bcbbc6b18cf1b3d2f259b294
Wieedze <maxime.moodz@gmail.com>
2025-06-06 11:21:21 +0200

    searchbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <a href="#constructor" className="nav-link">Constructor</a>
+        <a href="#about" className="nav-link">About</a>
+        <a href="#contact" className="nav-link">Contact</a>
+      </div>
+      <div className="navbar-right">
+        <input type="search" placeholder="Search..." className="search-input" />
+      </div>
+    </nav>
+  );
+}
